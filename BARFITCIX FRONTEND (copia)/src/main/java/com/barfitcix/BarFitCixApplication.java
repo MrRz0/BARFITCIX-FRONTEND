bc1d854b3b3d42c2abac602c5dae737efbe350b8
Moisés Ruiz <moisesruiz041@gmail.com>
2025-06-11 18:28:08 -0500

    Fronted de BarFitCIX

---
+++ BARFITCIX FRONTEND (copia)/src/main/java/com/barfitcix/BarFitCixApplication.java
@@ -0,0 +1,11 @@
+package com.barfitcix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarFitCixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BarFitCixApplication.class, args);
+    }
+}
